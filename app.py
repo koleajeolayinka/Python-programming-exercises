2182ecb44b1fe320725974d190341b0294469032
KOLEAJEOLAYINKA <100846558+KOLEAJEOLAYINKA@users.noreply.github.com>
2022-04-13 09:24:12 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,182 @@
+# print("koleaje olayinka")
+# print("o----")
+# print(" |||| ")
+# print('*' * 1)
+# print('*' * 2 )
+# print('*' * 3)
+# print('*' * 4)
+# print('*' * 5)
+# print('*' * 6)
+
+# price = 10
+# rating = 4.9
+# name = "olayinka"
+# isPublished = False
+# print(price)
+
+# name = "John Smith"
+# age = 20
+# isNewPatient = True
+# isOldPatient = False
+# name =    input('What is your name? ')
+# print('Hi ' + name)
+#
+# name = input("what is your name? ")
+# color = input("what is your favourite color? ")
+# input(name + ' likes ' + color)
+
+# int()
+# float()
+# bool()
+        #BIRTH
+#
+
+# first_integer = 1
+# second_integer = 2
+# thrid_integer = 3
+# forth_integer = 4
+# print(' 1 + 2 = ' ,first_integer + second_integer )
+# print('1 + 2 + 3 =', first_integer + second_integer + thrid_integer )
+# print("1 + 2 + 3 + 4 = " , first_integer + second_integer + thrid_integer + forth_integer)
+
+# first_integer = 1
+# second_integer = 2
+# thrid_integer = 3
+# forth_integer = 4
+# input(' 1 + 2 = ' ,)
+# input(first_integer + second_integer)
+# input('1 + 2 + 3 =',)
+# input(first_integer + second_integer + thrid_integer)
+# print("1 + 2 + 3 + 4 = " ,)
+# print(first_integer + second_integer + thrid_integer + forth_integer)
+
+#
+# userInput1 = input('enter first num    ber = ',)
+# firstNumber = int(userInput1)
+# userInput2 = input('enter second number = ',)
+# secondNumber = int(userInput2)
+# userInput3 = input('enter third number = ',)
+# thirdNumber = int(userInput3)
+# userInput4 = input('enter forth number = ',)
+# forthNumber = int(userInput3)
+#
+# print("1 + 2 =",firstNumber + secondNumber)
+# print("1 + 2 + 3 =",firstNumber + secondNumber + thirdNumber)
+# print("! + 2 + 3 + 4 =",firstNumber + secondNumber + thirdNumber + forthNumber)
+#
+#
+# digit0 = 0
+# square0 = digit0 * digit0
+# cube0 = digit0 * digit0 * digit0
+#
+# digit1 = 1
+# square1 = digit1 * digit1
+# cube1 = digit1 * digit1 * digit1
+#
+# digit2 = 2
+# square2 = digit2 * digit2
+# cube2 = digit2 * digit2 * digit2
+#
+# digit3 = 3
+# square3 = digit3 * digit3
+# cube3 = digit3 * digit3 * digit3
+#
+# digit4 = 4
+# square4 = digit4 * digit4
+# cube4 = digit4 * digit4 * digit4
+#
+# digit5 = 5
+# square5 = digit5 * digit5
+# cube5 = digit5 * digit5 * digit5
+#
+# print('number', 'square', 'cube', sep= '\t\t')
+# print('0', square0, cube0, sep= '\t\t\t')
+# print('1', square1, cube1, sep= '\t\t\t')
+# print('2', square2, cube2, sep= '\t\t\t')
+# print('3', square3, cube3, sep= '\t\t\t')
+# print('4', square4, cube4, sep= '\t\t\t')
+# print('5', square5, cube5, sep= '\t\t\t')
+
+# n_str = input('enter a number')
+# n = int(n_str)
+# if n%2==0:
+#     print("it is even")
+# else:
+#     print("odd")
+#
+# ok= input('number')
+# ok= int(n)
+# if n%3 ==0:
+#     print('odd')
+
+#
+# firstDigit = 30
+# secondDigit = 40
+# if firstDigit > secondDigit:
+#     print("the first digit is bigger")
+# else:
+#     print("the second digit is bigger")
+
+
+# grade = input('enter score')
+# grade = int(grade)
+# if grade >= 70 and grade <=100:
+#     print('a')
+#
+# if grade >= 60 and grade <=69:
+#     print('b')
+#
+# if grade >= 50 and grade <=59:
+#         print('c')
+#
+# if grade >= 40  and grade <= 49:
+#             print('d')
+#
+# if grade < 40:
+#     print('f')
+#
+# if grade > 100:
+#     print('null')
+#
+#
+
+# number = 0
+# while number <= 10:
+#   print(number)
+#   number+=1 #number of addition
+
+
+# number = 0
+#
+# while number <= 10:
+#
+#   print(number, end=' ')
+#
+#   number=number + 1 #Total number of addition
+#
+# print()
+# print("final value of number: ", number)
+
+
+# word = 'hello'
+# letter = ('h', 'e', 'l', 'l', 'o',)
+# for letter in word:
+#     print(letter.upper())
+
+# Name = 'John Smith'
+# print(Name)
+# age = 20
+# print(age)
+# is_new = True
+# is_old = False
+# name = input("what is your name ")
+# color = input("what is your favourite clor ")
+# print( name +" likes " + color)
+
+weight = input("what's your weight ")
+mass = double(weight) // 2205
+print("your mass in kilograms is " )
+print(mass)
+
+
+
