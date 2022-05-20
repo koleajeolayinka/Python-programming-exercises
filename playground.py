ae9d0a50a1273a90e25326ec6740214876b91511
olayinka <koleajeolayinka@gmail.com>
2022-05-20 11:39:07 +0100

    commit code

---
+++ playground.py
@@ -0,0 +1,5 @@
+class Playground:
+    def __init__(self, first_name, last_name, age):
+        self._age = age
+        self._name = f"{first_name} {last_name}"
+p1 =
--- playground_test.py
+++ playground_test.py
@@ -4,12 +4,16 @@ from . import playground
 
 class PlaygroundTest(unittest.TestCase):
     @classmethod
-    def setUp(cls) -> None:
+    def setUpClass(cls) -> None:
         print("i run before")
 
-    def tearDownClass(self) -> None:
+    def tearDown(self) -> None:
         print("I run after")
 
+    def test_something1(self):
+        print("i am running on my own-> 1 ")
 
+    def test_something2(self):
+        print("i am running on my own -> 2")
 if __name__ == '__main__':
     unittest.main()
