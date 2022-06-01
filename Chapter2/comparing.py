f95d98d038368d219c3877c8b7b74647fa4d21af
olayinka <koleajeolayinka@gmail.com>
2022-06-01 10:33:27 +0100

    commit code

---
+++ Chapter2/comparing.py
@@ -0,0 +1,18 @@
+number = int(input("Enter your first number: "))
+number2 = int(input("Enter your second number: "))
+if number == number2:
+    print(number, "and", number2, "are equal")
+
+if number > number2:
+    print(number, "is greater than", number2)
+
+if number >= number2:
+    print(number, "is greater than or equals to", number2)
+
+if number < number2:
+    print(number, "less than", number2)
+if number <= number2:
+    print(number, "less than or equals to", number2)
+
+if number != number2:
+    print(number, "and", number2, "are not equal")
