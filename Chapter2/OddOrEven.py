f13b1406d66fa0ce5d0eafd28034086d11232c5b
Koleaje Olayinka <koleajeolayinka@gmail.com>
2022-06-15 00:30:57 +0100

    commit code

---
+++ Chapter2/OddOrEven.py
@@ -0,0 +1,7 @@
+print("WELCOME TO ODD AND EVEN CONFIRMATION ")
+print("ENTER A DIGIT")
+number = int(input())
+if number % 2 == 0:
+    print(number, " IS EVEN NUMBER")
+elif number % 1 == 0:
+    print(number, "IS ODD NUMBER")
